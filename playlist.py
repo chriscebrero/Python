e0a047d41125d9a0791e0fe77527b5d1cc14adad
chriscebrero <chris.cebrero@gmail.com>
2018-08-10 21:17:32 -0700

    total up duration

--- playlist.py
+++ playlist.py
@@ -3,5 +3,12 @@ playlist = {'title': 'patagonia bus',
  			'songs': [
  			{'title': 'song1', 'artist': ['blue'], 'duration': 2.5},
  			{'title': 'song2', 'artist': ['kitty, djcat'], 'duration': 5.25},
- 			]
+ 			{'title': 'meow meow', 'artist': ['garfield'], 'duration': 2.0},
+ 		]
  }
+
+
+total_length = 0;
+ for song in playlist['songs']:
+ 	total_length += song['duration']
+ 	print(total_length)
