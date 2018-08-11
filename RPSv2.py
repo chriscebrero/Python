f8db4341c1efb7bec0400ecef00eefc1407b50cb
chriscebrero <chris.cebrero@gmail.com>
2018-08-10 20:22:05 -0700

    added no cheating block

--- RPSv2.py
+++ RPSv2.py
@@ -3,6 +3,7 @@ print("Paper...")
 print("Scissors!!")
 
 player1 = input("Player 1, make your move: ")
+print("***NO CHEATING***" * 20)
 player2 = input("Player 2, make your move: ")
 
 if player1 == player2:
