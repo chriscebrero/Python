a544eefb6dbe1e1fd649efde65f5ce8e80452b1d
chriscebrero <chris.cebrero@gmail.com>
2018-08-10 21:13:03 -0700

    spotify example

---
+++ playlist.py
@@ -0,0 +1,7 @@
+playlist = {'title': 'patagonia bus',
+ 			'author': 'colt steele',
+ 			'songs': [
+ 			{'title': 'song1', 'artist': ['blue'], 'duration': 2.5},
+ 			{'title': 'song2', 'artist': ['kitty, djcat'], 'duration': 5.25},
+ 			]
+ }
