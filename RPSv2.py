1a2c0cc78174c7313373d83fc6e2d9925c8fe4ff
chriscebrero <chris.cebrero@gmail.com>
2018-08-10 20:20:13 -0700

    added 2nd version, nested conditionals

---
+++ RPSv2.py
@@ -0,0 +1,27 @@
+print("Rock...")
+print("Paper...")
+print("Scissors!!")
+
+player1 = input("Player 1, make your move: ")
+player2 = input("Player 2, make your move: ")
+
+if player1 == "rock":
+	if player2 == "scissors":
+		print("Player 1 Wins")
+	elif player2 == "paper":
+		print("Player 2 Wins")
+elif player1 == "paper":
+	if player2 == "rock":
+		print("Player 1 Wins")
+	elif player2 == "scissors":
+		print("Player 2 Wins")
+elif player1 == "scissors":
+	if player2 == "paper":
+		print("Player 1 Wins")
+	elif player2 == "rock":
+		print("Player 2 Wins")
+elif player1 == player2:
+	print("It's a tie...Unbelievable!")
+else:
+	print("Start over!")
+
