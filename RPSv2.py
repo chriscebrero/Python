6d8fb432ad30d284e140c97ca62101ebb6e53916
chriscebrero <chris.cebrero@gmail.com>
2018-08-10 20:21:01 -0700

    put tie at top

--- RPSv2.py
+++ RPSv2.py
@@ -5,7 +5,9 @@ print("Scissors!!")
 player1 = input("Player 1, make your move: ")
 player2 = input("Player 2, make your move: ")
 
-if player1 == "rock":
+if player1 == player2:
+	print("It's a tie...Unbelievable!")
+elif player1 == "rock":
 	if player2 == "scissors":
 		print("Player 1 Wins")
 	elif player2 == "paper":
@@ -20,8 +22,6 @@ elif player1 == "scissors":
 		print("Player 1 Wins")
 	elif player2 == "rock":
 		print("Player 2 Wins")
-elif player1 == player2:
-	print("It's a tie...Unbelievable!")
 else:
 	print("Start over!")
 
